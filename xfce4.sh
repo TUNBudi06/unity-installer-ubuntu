b55e59dc0bf400fe7cd3318f0eeb4b1cbc4da656
TUNBudi06 <70352065+TUNBudi06@users.noreply.github.com>
2022-07-10 21:12:24 +0700

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -5,4 +5,8 @@ apt install dialog build-essential -y
 apt install udisks2 -y
 rm -rf /var/lib/dpkg/info/udisks2.postinst
 dpkg --configure -a
-apt install xubuntu-desktop -y
+apt install xubuntu-desktop tigervnc-standalone-server -y
+apt install gnome-icon-theme
+apt install tigervnc-xorg-extension
+apt install mesa-utils- *
+apt instal x11- *
